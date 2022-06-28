f9183304a78c853765dc9edd76463554d93834b5
Godwin Enahoro <giogdxgas1@gmail.com>
2022-06-28 12:56:14 -0700

    positive or negative commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number < 0:
+    print(number,' is negative\n')
+elif number == 0:
+    print(number, ' is zero\n')
+else:
+    print(number, 'is positive\n')
+
